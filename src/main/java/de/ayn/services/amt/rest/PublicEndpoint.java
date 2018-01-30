7963a2aeaedf5ab8635913734c505030988698c4
Sergej Samsonow <sergej.samsonow@allyouneedfresh.de>
2018-01-30 14:08:55 +0100

    Add greeting method

--- src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
+++ src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
@@ -2,9 +2,11 @@ package de.ayn.services.amt.rest;
 
 import static java.util.Collections.emptyList;
 import java.util.Map;
+import java.util.Optional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,13 @@ public class PublicEndpoint {
     @Path("version")
     public Result<Map<String, Object>>  version() {
         return new Result<>(Result.State.OK, service.simpleVersionInfo(), emptyList());
+    }
+    
+    @GET
+    @Path("greeting/{name}")
+    public Result<String> greeting(@PathParam("name") String name) {
+        String message = "Hallo " + Optional.ofNullable(name).orElse("anonymous");
+        return new Result<>(Result.State.OK, message, emptyList());
     }
 
 }
