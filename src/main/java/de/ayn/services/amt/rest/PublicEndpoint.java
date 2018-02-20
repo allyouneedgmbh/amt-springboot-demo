a364ac79c8c970ac324bc44666d514276a36f16a
Sergej Samsonow <sergej.samsonow@allyouneedfresh.de>
2018-02-20 09:19:30 +0100

    Add auth

--- src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
+++ src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
@@ -1,13 +1,20 @@
 package de.ayn.services.amt.rest;
 
 import static java.util.Collections.emptyList;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Formatter;
 import java.util.Map;
 import java.util.Optional;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -46,7 +53,21 @@ public class PublicEndpoint {
     public Result<String> next3method() {
         return new Result<>(Result.State.OK, "next-3-method", emptyList());
     }
-
-
-
+    
+    @GET
+    @Path("auth")
+    public Result<String> auth(@QueryParam("data") String data) {
+        SecretKeySpec key = new SecretKeySpec("DiesIstEinTest".getBytes(), "HmacSHA1");
+        try (Formatter formatter = new Formatter()) {
+            Mac mac = Mac.getInstance("HmacSHA1");
+            mac.init(key);
+            for (byte current : mac.doFinal(data.getBytes())) {
+                formatter.format("%02x", current);
+            }
+            return new Result<>(Result.State.OK, formatter.toString(), emptyList());
+        }
+        catch (RuntimeException | InvalidKeyException | NoSuchAlgorithmException e) {
+            return new Result<>(Result.State.ERROR,null, Arrays.asList(e.getMessage()));
+        }
+    }
 }
