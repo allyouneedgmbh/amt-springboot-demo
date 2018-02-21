2cb7a1d4c6361170ef0897a2dabe13d408b1e734
Sergej Samsonow <sergej.samsonow@allyouneedfresh.de>
2018-02-21 11:11:37 +0100

    Amt test 3

--- src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
+++ src/main/java/de/ayn/services/amt/rest/PublicEndpoint.java
@@ -76,7 +76,7 @@ public class PublicEndpoint {
     @GET
     @Path("staticString")
     public Result<String> getStaticString() {
-        return new Result<>(Result.State.OK,"TEST 2",Collections.emptyList());
+        return new Result<>(Result.State.OK,"TEST 3",Collections.emptyList());
     }
 
     
